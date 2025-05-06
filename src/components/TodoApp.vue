3eee3a0ef37882b8c1457980fc4579e47482ab7c
nailaaaa12 <nailanatasya@student.uir.ac.id>
2025-05-06 18:29:26 +0700

    Struktur Awal

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue';
+
+const activities = ref([]);
+const newActivity = ref('');
+</script>
+
+<template>
+  <div class="page-wrapper">
+    <div class="container">
+      <h2 class="title">Daftar Tanaman</h2>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
