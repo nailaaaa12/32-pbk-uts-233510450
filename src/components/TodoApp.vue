51d5a229aa3935abc2f9216a49a05fa7c55c62a3
nailaaaa12 <nailanatasya@student.uir.ac.id>
2025-05-06 18:34:38 +0700

    Menambahkan Fungsi Pada Fitur Tambah

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -3,6 +3,13 @@ import { ref } from 'vue';
 
 const activities = ref([]);
 const newActivity = ref('');
+
+const addActivity = () => {
+  if (newActivity.value.trim()) {
+    activities.value.push({ text: newActivity.value, completed: false });
+    newActivity.value = '';
+  }
+};
 </script>
 
 <template>
@@ -11,6 +18,11 @@ const newActivity = ref('');
       <h2 class="title">Daftar Tanaman</h2>
     </div>
   </div>
+
+  <div class="input-container">
+  <input v-model="newActivity" class="input-field" placeholder="Tambahkan tanaman baru" />
+  <button @click="addActivity" class="add-button">Tambah</button>
+</div>
 </template>
 
 <style scoped>
